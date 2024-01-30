e2d6c1830b6ed0a27cb49cfa961cbc1598a6b2f0
Lysepten <tlstlsrbrb11@gmail.com>
2024-01-30 15:21:22 +0900

    회원가입시 중복체크, null체크, 문장 구성을 쉽게 할 수 있도록

--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -4,8 +4,25 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import com.example.demo.vo.Member;
+
 @Mapper
 public interface MemberRepository {
+	@Select("""
+			SELECT *
+			FROM `member`
+			WHERE loginId = #{loginId}
+			""")
+	public Member getMemberByLoginId(String loginId);
+
+	@Select("""
+			SELECT *
+			FROM `member`
+			WHERE name = #{name} 
+			AND email = #{email}
+			""")
+	public Member getMemberByNameAndEmail(String name, String email);
+
 	@Insert("""
 			INSERT INTO
 			`member` SET
@@ -13,7 +30,7 @@ public interface MemberRepository {
 			updateDate = NOW(),
 			loginId = #{loginId},
 			loginPw = #{loginPw},
-			name = #{name},
+			`name` = #{name},
 			nickname = #{nickname},
 			cellphoneNum = #{cellphoneNum},
 			email = #{email}
@@ -22,4 +39,8 @@ public interface MemberRepository {
 
 	@Select("SELECT LAST_INSERT_ID()")
 	public int getLastInsertId();
+
+	@Select("SELECT * FROM `member` WHERE id = #{id}")
+	public Member getMember(int id);
+
 }
