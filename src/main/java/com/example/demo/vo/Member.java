a098ce4c83e2e935d28c71212a7902f07eadd4a9
Lysepten <tlstlsrbrb11@gmail.com>
2024-01-30 10:19:01 +0900

    회원가입 기능 구현, 회원 테이블 추가

---
+++ src/main/java/com/example/demo/vo/Member.java
@@ -0,0 +1,18 @@
+package com.example.demo.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+	int id;
+	String loginId;
+	String loginPw;
+	String name;
+	String nickname;
+	String cellphoneNum;
+	String email;
+}
