171790038221fb52b94a062214a3fb58060274e7
Lysepten <tlstlsrbrb11@gmail.com>
2024-02-05 10:55:11 +0900

    회원가입 폼 제작, 회원가입 처리 구현

--- src/main/java/com/example/demo/MyWebMVCConfigurer.java
+++ src/main/java/com/example/demo/MyWebMVCConfigurer.java
@@ -19,7 +19,8 @@ public class MyWebMVCConfigurer implements WebMvcConfigurer {
 	// NeedLoginInterceptor 불러오기(연결)
 	@Autowired
 	NeedLoginInterceptor needLoginInterceptor;
-	
+
+	// NeedLogoutInterceptor 불러오기(연결)
 	@Autowired
 	NeedLogoutInterceptor needLogoutInterceptor;
 
@@ -30,8 +31,11 @@ public class MyWebMVCConfigurer implements WebMvcConfigurer {
 
 		registry.addInterceptor(needLoginInterceptor).addPathPatterns("/usr/article/write")
 				.addPathPatterns("/usr/article/doWrite").addPathPatterns("/usr/article/modify")
-				.addPathPatterns("/usr/article/doModify").addPathPatterns("/usr/article/doDelete")
-				.addPathPatterns("/usr/member/doLogout");
+				.addPathPatterns("/usr/article/doModify").addPathPatterns("/usr/article/doDelete");
+
+		registry.addInterceptor(needLogoutInterceptor).addPathPatterns("/usr/member/login")
+				.addPathPatterns("/usr/member/doLogin").addPathPatterns("/usr/member/join")
+				.addPathPatterns("/usr/member/doJoin");
 	}
 
 }
